cbd98580703a669ee28940b6ed7429ed6a142832
Luke Fry <luke.fry096@gmail.com>
2021-04-16 11:16:42 +1000

    shop listing archive page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './page/homepage/homepage.component';
+import ShopPage from './page/shoppage/shoppage.component';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/shop/" component={ShopPage}></Route>
+      </Switch>
     </div>
   );
 }
